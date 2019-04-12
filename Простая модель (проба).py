d7daef151cb9fbf9841a1ae7c14077f19bce5458
Kedo-Aleksei <49563474+Kedo-Aleksei@users.noreply.github.com>
2019-04-12 18:57:25 +0300

    Исправил сумму показателей - скорректировал на количество раз, что они встречаются
    Точно так же я исправил модель Никиты в экселе в прошлый раз.

--- Простая модель (проба).py
+++ Простая модель (проба).py
@@ -1,4 +1,4 @@
-fin = open('1981.csv', encoding='utf8')
+fin = open('1981.csv')
 A = list(fin)
 A.pop(0)
 A.pop(0)  # Удаляем первые две строки
@@ -8,10 +8,15 @@ for s in A:
     s = s.split(';')
     data.append(list(map(int, s)))
 # Считаны данные
-M = [0] * 36
+M = [0] * 36  # Список суммы показателей
+B = [0] * 36  # Количество раз, когда выпал этот элемент
 for i in range(len(data)):
     for j in [1, 2, 3, 4, 5]:
         M[data[i][j] - 1] += data[i][7] * 10000000 / data[i][6]
+        B[data[i][j] - 1] += 1
+# Исправляем список суммы показателей
+for i in range(36):
+    M[i] /= B[i]
 k = 0
 K = [0] * 36  # Проценты
 for i in range(36):
