411c647dc9d9f0dd5405958fccec8562af9f2d48
Kedo-Aleksei <49563474+Kedo-Aleksei@users.noreply.github.com>
2019-04-24 22:53:20 +0300

    Подсчет вероятностей превратил в функцию
    Есть коррекция на сумму показателей

--- twofiles.py
+++ twofiles.py
@@ -1,24 +1,43 @@
-B = [[0] * 36, [0] * 36]
-for e in range(1, 3):
-    lot = open('{id}.csv'.format(id=e))
-    A = list(lot)
+def pos(fin):
+    A = list(fin)
     A.pop(0)
-    A.pop(0)
+    A.pop(0)  # Удаляем первые две строки
     data = []
     for s in A:
-        s = s[:-1].replace(" ", "")
+        s = s[:-1].replace(" ", "")  # Удаляем \n
         s = s.split(';')
         data.append(list(map(int, s)))
-    M = [0] * 36
+    # Считаны данные
+    M = [0] * 36  # Список суммы показателей
+    B = [0] * 36  # Количество раз, когда выпал этот элемент
     for i in range(len(data)):
         for j in [1, 2, 3, 4, 5]:
             M[data[i][j] - 1] += data[i][7] * 10000000 / data[i][6]
+            B[data[i][j] - 1] += 1
+    # Исправляем список суммы показателей
+    for i in range(36):
+        M[i] /= B[i]
     k = 0
-    K = [0] * 36
+    K = [0] * 36  # Проценты
     for i in range(36):
         k += M[i]
     for i in range(36):
-        K[i] = M[i] * 100 / k
-    for i in range(36):
-        B[e-1][i] = K[i]
+        K[i] = 100 * M[i] / k
+    return K
+
+
+B = []
+lot = open('1.csv')
+B.append(pos(lot))
+lot = open('2.csv')
+B.append(pos(lot))
 print(B)
+
+C = [0] * 36
+for i in range(36):
+    C[i] = (B[0][i] + B[1][i]) / 2
+print(C)
+D = [0] * 36
+for i in range(36):
+    D[i] = ((B[0][i] - C[i])**2 + (B[1][i] - C[i])**2) / 2
+print(D)
