2660faf578d445209a7e5441526a6459a6bfaa5d
Karpov-Nikita <48890730+Karpov-Nikita@users.noreply.github.com>
2019-05-21 01:03:39 +0400

    Created one graph instead of two.

--- twoperiods_AlexVersion.py
+++ twoperiods_AlexVersion.py
@@ -74,14 +74,8 @@ for i in range(36):
 print('Максимальное из стандартных отклонений')
 print(x)
 
-plt.plot(C1, "bo")
-plt.title("Средние выборочные 1 периода")
-plt.ylabel('Вероятность')
+plt.plot(C1, "bo", С2, "go")
+plt.title("Средние выборочные 1 (син.) и 2 (зел.) периодов")
 plt.xlabel('Номер')
-plt.show()
-
-plt.plot(C2, "bo")
-plt.title("Средние выборочные 2 периода")
-plt.ylabel('Вероятность')
-plt.xlabel('Номер')
+plt.ylabel('Вероятность (%)')
 plt.show()
