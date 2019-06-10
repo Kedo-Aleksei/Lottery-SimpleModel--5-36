22e26a503b3dd857123c6527c09245233362a3b9
Ksenia <xenia.kuvshinova@yandex.ru>
2019-06-10 20:33:52 +0300

    Исправила scratch - код для составления матрицы Улама для последовательности от 1 до n

--- scratch.py
+++ scratch.py
@@ -1,49 +1,39 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from itertools import cycle
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-df = pd.read_csv(url
-                 , names=['sepal length','sepal width','petal length','petal width','target'])
-print(df.head())
+n = 5
 
-features = ['sepal length', 'sepal width', 'petal length', 'petal width']
-x = df.loc[:, features].values
-y = df.loc[:,['target']].values
-x = StandardScaler().fit_transform(x)
-pd.DataFrame(data = x, columns = features).head()
-print(x)
-print(y)
+directions = ((1, 0), (0, -1), (-1, 0), (0, 1))
 
-pca = PCA(n_components=2)
-principalComponents = pca.fit_transform(x)
-principalDf = pd.DataFrame(data = principalComponents
-             , columns = ['principal component 1', 'principal component 2'])
-
-principalDf.head(5)
-df[['target']].head()
-finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
-finalDf.head(5)
-
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(1,1,1)
-ax.set_xlabel('Principal Component 1', fontsize = 15)
-ax.set_ylabel('Principal Component 2', fontsize = 15)
-ax.set_title('2 Component PCA', fontsize = 20)
+nums = range(1, n * n + 1)
 
 
-targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-colors = ['r', 'g', 'b']
-for target, color in zip(targets,colors):
-    indicesToKeep = finalDf['target'] == target
-    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-               , finalDf.loc[indicesToKeep, 'principal component 2']
-               , c = color
-               , s = 50)
-ax.legend(targets)
-ax.grid()
-plt.show()
+def length_producer(_max):
+    curr_l = _max
+    while curr_l > 0:
+        yield curr_l
+        curr_l -= 1
+        yield curr_l
 
-print(pca.explained_variance_ratio_)
+
+print(list(length_producer(n)), sum(list(length_producer(n))))
+
+buffer = []
+for i in range(n):
+    buffer.append([None] * n)
+
+print(buffer)
+
+curr_i, curr_j = -1, n - 1
+
+values_iterator = iter(nums)
+
+for l, d in zip(length_producer(n), cycle(directions)):
+    print(l, d)
+    for i in range(l):
+        num = next(values_iterator)
+        curr_i += d[0]
+        curr_j += d[1]
+        buffer[curr_i][curr_j] = num
+
+for line in buffer:
+    print(line)
