9385feab515707da183ab7f2546419ffe2a0b728
Ksenia <xenia.kuvshinova@yandex.ru>
2019-06-17 04:20:42 +0300

    Update Ulam_seaborn.py
    ура!

--- Ulam_seaborn.py
+++ Ulam_seaborn.py
@@ -1,7 +1,12 @@
 import csv
 import numpy as np
 import pip
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib_colorbar as pltc
 
-pip install seaborn
+A = pd.read_csv('matrix.csv').values
 
-A = np.array(open('matrix.csv'))
+sns.heatmap(A, vmin=0, vmax=1, cmap = 'winter', center=None, robust=False, annot=None, fmt='.2g', annot_kws=None, linewidths=0, linecolor='white', cbar=True, cbar_kws=None, cbar_ax=None, square=False, xticklabels='auto', yticklabels='auto', mask=None, ax=None)
+plt.show()
